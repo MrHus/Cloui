a60eb17d4818fc5428fdad7b8099ca92dd4bc26f
Maarten Hus <MaartenHus@gmail.com>
2011-01-10 22:32:56 +0100

    updated readme, changed project name to lowercase

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject Cloui "1.0"
+(defproject cloui "1.0"
 	:dependencies [[org.clojure/clojure "1.2.0"] 
-	               [org.clojure/clojure-contrib "1.2.0"]])
+	               [org.clojure/clojure-contrib "1.2.0"]]
+	:dev-dependencies [lein-clojars "0.5.0-SNAPSHOT"])
